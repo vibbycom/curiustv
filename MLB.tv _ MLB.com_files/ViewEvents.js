d81edd9b7721fd2122cf0a269fbe66093d6cecd9
Neven <neven.vuger@gmail.com>
2018-03-12 14:55:23 +0100

    one click mlb

---
+++ MLB.tv _ MLB.com_files/ViewEvents.js
@@ -0,0 +1,48 @@
+define({
+    GAMEDAYREADY: "GamedayApptvReady",
+    SHOW_SCORDBOARD: "GamedayApptvScoreboardShow",
+    HIDE_SCORDBOARD: "GamedayApptvScoreboardHide",
+    INIT_SCORDBOARD: "GamedayApptvScoreboardInit",
+    HIGHLIGHT_GAMES_SCORDBOARD: "GamedayApptvScoreboardHighLightGames",
+
+    SHOW_CHYRON: "GamedayApptvChyronShow",
+    HIDE_CHYRON: "GamedayApptvChyronHide",
+    INIT_CHYRON: "GamedayApptvChyronInit",
+    READY_CHYRON: "GamedayApptvChyronReady",
+
+    SHOW_RIGHTPANEL: "GamedayApptvRightPanelShow",
+    HIDE_RIGHTPANEL: "GamedayApptvRightPanelHide",
+    INIT_RIGHTPANEL: "GamedayApptvRightPanelInit",
+
+
+    SHOW_STRIKEZONE: "GamedayApptvStrikeZoneShow",
+    HIDE_STRIKEZONE: "GamedayApptvStrikeZoneHide",
+    INIT_STRIKEZONE: "GamedayApptvStrikeZoneInit",
+
+
+    SCORDBOARD : {
+        "DATA" : {
+            "UPDATED" : "GamedayApptvScoreboardDataUpdated"
+        },
+        "GAME" : {
+            "CLICK" : "GamedayApptvScoreboardGameClicked",
+            "HOVER" : "GamedayApptvScoreboardGameHover"
+        }
+    },
+
+    CHYRON : {
+        LINESCORE: {
+            CLICK : "GamedayLinescoreClicked"
+        }
+    },
+
+    GAMEDAY : {
+        GAME: {
+            SWITCH : "GamedayGameSwitched"
+        },
+        DATA: {
+            UPDATED : "GamedayDataUpdated"
+        }
+
+    }
+});
